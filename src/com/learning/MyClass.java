37bdb94a2fe4f250a56a7bfe381c058287813a20
MayankSingh6715 <95568044+MayankSingh6715@users.noreply.github.com>
2022-02-24 10:41:05 +0530

    Inner Class

--- src/com/learning/MyClass.java
+++ src/com/learning/MyClass.java
@@ -10,8 +10,14 @@ public class MyClass extends PhoneBrand {
         phone obj1 = new MyClass("Mi");
         phone obj2 = new MyClass("Samsung");
 
+//      Interface Implementation
         obj.displayName();
         obj1.displayName();
         obj2.displayName();
+        
+//      inner class implementation
+        InnerClass outter = new InnerClass();
+        InnerClass.inner1 inner = outter.new inner1();
+        inner.alert();
     }
 }
